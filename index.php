70d8c691cb8e0554bb2c5e41fbe72ba2ec9ad5d4
FaithMcElhaney <Faith.McElhaney@students.jmcss.org>
2021-09-20 11:31:11 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>faith</title>
+</head>
+
+<body>
+
+<?php
+$name="Cindy just pulled a whole thing of cookies out of her backpack. ";
+echo $name;
+?>
+    
+</body>
+
+</html>
